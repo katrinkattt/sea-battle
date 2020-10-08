9091a8ea312eb150652b0a29f18c9c5cfb1c448a
Nkitina Ekaterina <52584077+katrinkattt@users.noreply.github.com>
2020-10-08 16:14:17 +0500

    web-app build

---
+++ build/precache-manifest.8c37814c044bf59d0d514b8ab4bd9e82.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4793ba93b37d8010f3c12a285e34627b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "feafa0a7cf64a90352eb",
+    "url": "/static/css/main.4836be33.chunk.css"
+  },
+  {
+    "revision": "00f1bcd853443006899b",
+    "url": "/static/js/2.9d372db0.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/static/js/2.9d372db0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "feafa0a7cf64a90352eb",
+    "url": "/static/js/main.c5acda5a.chunk.js"
+  },
+  {
+    "revision": "16c0d9c8f280eb93af32",
+    "url": "/static/js/runtime-main.aba22706.js"
+  }
+]);
